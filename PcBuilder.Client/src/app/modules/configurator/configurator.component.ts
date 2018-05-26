dae7334047b3aca23e4d3030bd3ea826f5be44ef
Bordeianu Razvan <bordeianu_razvan_2006@yahoo.com>
2018-05-26 16:58:45 +0300

    made config navbar responsive

--- PcBuilder.Client/src/app/modules/configurator/configurator.component.ts
+++ PcBuilder.Client/src/app/modules/configurator/configurator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-configurator',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configurator.component.css']
 })
 export class ConfiguratorComponent implements OnInit {
-  constructor() {}
+  exitConfig() {
+    console.log('button pressed');
+      this.router.navigate(['/']);
+  }
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 }
