1224e1a0029f779748f17f0720004b7a6e1b9adb
Bordeianu Razvan <bordeianu_razvan_2006@yahoo.com>
2018-06-02 11:32:04 +0300

    added tutorial page

--- PcBuilder.Client/src/app/modules/core/core.module.ts
+++ PcBuilder.Client/src/app/modules/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { RouterModule } from '@angular/router';
+import { TutorialComponent } from './components/tutorial/tutorial.component';
 
 @NgModule({
   imports: [
@@ -9,6 +10,6 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   exports: [],
-  declarations:  [HomeComponent]
+  declarations:  [HomeComponent, TutorialComponent]
 })
 export class CoreModule { }
